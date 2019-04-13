d8b920f5eb7bae23650a3ce826d1969b6b567786
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-04-13 11:40:26 -0500

    ajax  request added

---
+++ asset/code.js
@@ -0,0 +1,20 @@
+
+var ingredient="tomato";
+var key= "d700cd0ee0b7bf70739c9bd846d3080d"	;	
+        var queryURL = "https://api.edamam.com/search?q=" +
+        ingredient + "&app_id=" +  "f1e5b0de" + "&app_key=" + key;
+
+
+
+        console.log(queryURL)
+// metodo ajax  para objetener de la url lo que quermos
+      $.ajax({
+        url: queryURL, 
+        method: "GET"
+      })
+      // hace una promesa de que se cargue hasta que lo de arriba este listo o no se ejecute mientras
+
+        .then(function(response) {
+
+            
+        });
