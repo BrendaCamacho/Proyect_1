2a01db31b4174d1dd503e17336b15941c625691c
Charlie <caam_1275@hotmail.com>
2019-04-30 01:39:09 -0500

    Contador visitas sin estilo, 12 resultados, favicon sintetizado para leerse

--- asset/js/code.js
+++ asset/js/code.js
@@ -1,38 +1,36 @@
 $(document).ready(function(){
     // ===firebase===
 
-    var config = {
-        apiKey: "AIzaSyCpq0tQ_Vkd6lMOA4s7dWmoTn45cTzIwvc",
-        authDomain: "proyect1-27081.firebaseapp.com",
-        databaseURL: "https://proyect1-27081.firebaseio.com",
-        projectId: "proyect1-27081",
-        storageBucket: "proyect1-27081.appspot.com",
-        messagingSenderId: "998295644330"
-      };
+        var config = {
+            apiKey: "AIzaSyCpq0tQ_Vkd6lMOA4s7dWmoTn45cTzIwvc",
+            authDomain: "proyect1-27081.firebaseapp.com",
+            databaseURL: "https://proyect1-27081.firebaseio.com",
+            projectId: "proyect1-27081",
+            storageBucket: "proyect1-27081.appspot.com",
+            messagingSenderId: "998295644330"
+        };
       
       firebase.initializeApp(config);
       var contadorjs = 0;
 
           
            
-          var database = firebase.database();
-          var searchTerm = "";
-          var connectionsRef = database.ref("/connections");
-          var connectedRef = database.ref(".info/connected");
-              searchTerm = $("#ingredients").val().trim(); 
+        var database = firebase.database();
+        var searchTerm = "";
+        var connectionsRef = database.ref("/connections");
+        var connectedRef = database.ref(".info/connected");
+            searchTerm = $("#ingredients").val().trim(); 
 
-            database.ref().push({
-            searchTerm:searchTerm,
-            });  
+        database.ref().push({
+        searchTerm:searchTerm,
+        });  
 
 
           
 
         connectedRef.on("value", function(snap) {
-            console.log("Leyendo esto contd")
             if (snap.val()) {
             var con = connectionsRef.push(true);
-            console.log(con)
             console.log("guardar contador", contadorjs)
         
             }//Cierra If
@@ -43,6 +41,7 @@ $(document).ready(function(){
             console.log("actualizar contador")
             console.log(snap.val().contador)
             contadorjs = snap.val().contador
+            $(".visitas").text(contadorjs + " visitors");    
 
         });
 
@@ -50,9 +49,9 @@ $(document).ready(function(){
         connectionsRef.on("value", function(snapshot) {
             contadorjs ++ ;
             database.ref("contador").set({
-            contador: contadorjs
+                contador: contadorjs
             })
-            });
+        });
 
  
         // var ref = firebase.database().ref(searchTerm);
@@ -61,260 +60,257 @@ $(document).ready(function(){
         // // });
 
 
+    // ===== Global Variables =====
 
-var ingredientsArray = [];
-var addedIngredient = "";
-var ingredientsString="";
-
-
-// === Render buttons ===
-
-function renderButtons(){
-
-  $(".chipsRow").empty();
-
-  for (var i = 0; i < ingredientsArray.length; i++) {
-    var chipX = $("<div>");
-    chipX.addClass("chip");
-    chipX.attr("data-name", ingredientsArray[i]);
-    chipX.text(ingredientsArray[i]);
-    $(".chipsRow").append(chipX);
-  };
-
-  var closeX = $("<i>");
-  closeX.text("close");
-  closeX.addClass("close material-icons");
-  closeX.attr("data-name", ingredientsArray[i]);
-  $(".chip").append(closeX);
-
-};
-
-// ===== End Render Buttons =====
+    var ingredientsArray = [];
+    var addedIngredient = "";
+    var ingredientsString="";
 
 
-// ===== Add On click  ===== 
-$("#addIngredients").on("click", function(event) {
-  event.preventDefault();
-  addedIngredient = $("#ingredients").val().trim();
-  ingredientsArray.push(addedIngredient);
-  $("#ingredients").val("");
-  renderButtons();
-  console.log("ingredientes sin borrar", ingredientsArray);
+    // === Render buttons ===
+    function renderButtons(){
+    $(".chipsRow").empty();
 
-  ingredientsString = ingredientsArray.join("-");
-
-  $(".recipesContainer").empty();
-    
-  ajaxCall();
-
-      });
-// ===== Ends Add on click =====
-
-// ===== CLOSE CHIPS ===== 
-$(document).on("click", ".close", function(){
-
-    var parent = $(this).parent();
-    var name = parent.data("name");
-    console.log("CLick close");
-    var adios = ingredientsArray.splice(ingredientsArray.indexOf(name), 1);
-    console.log("adios", adios)    
-    console.log("despues de borrar", ingredientsArray)
-    console.log("ingredients strings", ingredientsString);
-    $(".recipesContainer").empty();
-    ajaxCall();
-});
-    
-
-
-
-// ====================== AJAX CALLS ===============================
-
-  function ajaxCall(){
-    ingredientsString = ingredientsArray.join("-");
-    var key= "d700cd0ee0b7bf70739c9bd846d3080d"	;	
-    var queryURL = "https://api.edamam.com/search?q=" +
-    ingredientsString + "&app_id=" +  "f1e5b0de" + "&app_key=" + key;
-      
-      
-      
-    console.log(queryURL);
-              
-  // metodo ajax  para objetener de la url lo que quermos
-    $.ajax({
-    url: queryURL, 
-    method: "GET"
-    })
-  // hace una promesa. de que se cargue hasta .que lo de arriba este listo o no se ejecute mientras
-    
-    .then(function(response) {
-
-    var results = response.hits;
-    //console.log("Results", results);
-
-     for (var i = 0; i < results.length; i++) {
-        // $(".recipesContainer").empty();
-
-      var cardContainer = $("<div>");
-          cardContainer.addClass("cardContainer col s12 m6 l4 xl3");
-          $(".recipesContainer").append(cardContainer);
-
-      var singleCard = $("<div>");
-          singleCard.addClass("card sticky-action col s12");
-          cardContainer.append(singleCard);
-          
-      var imageContainer = $("<div>");
-          imageContainer.addClass("card-image waves-effect waves-block waves-light"); 
-          singleCard.append(imageContainer);
-      
-          var image = response.hits[i].recipe.image;
-          //console.log(image);
-      
-      var imageCard = $("<img>");
-          imageCard.addClass("activator");
-          imageCard.attr("src", image);
-          imageContainer.append(imageCard);
-
-    // ===== CARD CONTENT =====
-
-      var cardContent = $("<div>");
-          cardContent.addClass("card-content");
-          singleCard.append(cardContent);    
-
-
-          var nameRecipe = response.hits[i].recipe.label;
-          //console.log(nameRecipe);
-       
-      var cardTitle = $("<div>");
-          cardTitle.addClass("cardTitle");
-          cardContent.append(cardTitle);
-
-      var nombre = $("<div>");
-          nombre.addClass("card-title activator truncate flexTitle");
-          nombre.text(nameRecipe);    
-          cardTitle.append(nombre);
-
-      var iconContainer = $("<div>");
-          iconContainer.addClass("card-title activator");
-          cardTitle.append(iconContainer);
-          
-      var iconoMas = $("<i>");
-          iconoMas.addClass("material-icons small right");
-          iconoMas.text("add_circle");
-          iconContainer.append(iconoMas); 
-
-          var cookingTime = response.hits[i].recipe.totalTime;
-          //console.log(cookingTime);
-
-      var cardTime = $("<div>");
-          cardTime.addClass("cardText");
-          cardContent.append(cardTime);
-
-         if (cookingTime === 0){
-            cookingTime = "N/A";
-            var time = $("<p>")
-            time.text(cookingTime);
-            cardTime.append(time);
-
-        }else{
-            var time = $("<p>")
-            time.text(cookingTime + " min");
-            cardTime.append(time);
+        for (var i = 0; i < ingredientsArray.length; i++) {
+            var chipX = $("<div>");
+            chipX.addClass("chip");
+            chipX.attr("data-name", ingredientsArray[i]);
+            chipX.text(ingredientsArray[i]);
+            $(".chipsRow").append(chipX);
         };
-          
 
-      
-      var iconoReloj = $("<i>");
-          iconoReloj.addClass("material-icons left");
-          iconoReloj.text("timer");
-          time.append(iconoReloj);
+        var closeX = $("<i>");
+        closeX.text("close");
+        closeX.addClass("close material-icons");
+        closeX.attr("data-name", ingredientsArray[i]);
+        $(".chip").append(closeX);
 
+    };
 
+    // ===== Add On click  ===== 
+    $("#addIngredients").on("click", function(event) {
+        event.preventDefault();
+        addedIngredient = $("#ingredients").val().trim();
+        ingredientsArray.push(addedIngredient);
+        $("#ingredients").val("");
+        renderButtons();
+        console.log("ingredientes sin borrar", ingredientsArray);
 
-       var calories = response.hits[i].recipe.calories;
-       var parseCalories = parseInt(calories);
-       //console.log(calories);
+        ingredientsString = ingredientsArray.join("-");
 
-      var cardCalories = $("<div>");
-           cardCalories.addClass("cardText");
-           cardContent.append(cardCalories);
+        $(".recipesContainer").empty();
+            
+        ajaxCall();
 
-      var calorias = $("<p>");
-          calorias.text(parseCalories + " cal");
-          cardCalories.append(calorias);
-          
-      var iconoFeliz = $("<i>");
-          iconoFeliz.addClass("material-icons left");
-          iconoFeliz.text("mood");
-          calorias.append(iconoFeliz);
-       
-    // =========== CARD REVEAL ============
+    }); 
 
-      var servings = response.hits[i].recipe.yield;
-       //console.log(servings);
-       
-      var reveal = $("<div>");
-          reveal.addClass("card-reveal");
-          singleCard.append(reveal);
-
-
-
-      var titleReveal = $("<div>");
-          titleReveal.addClass("card-title flexTitle truncate");
-          titleReveal.text(nameRecipe);
-          reveal.append(titleReveal);
-
-      var iconoContenedor = $("<div>");
-          iconoContenedor.addClass("card-title");
-          reveal.append(iconoContenedor);
-
-       
-      var closeReveal = $("<i>");
-          closeReveal.addClass("material-icons small right");
-          closeReveal.text("close");
-          iconoContenedor.append(closeReveal); 
-
-      var yieldServings = $("<p>");
-          yieldServings.addClass("yields");
-          yieldServings.text(servings + " servings");
-          reveal.append(yieldServings);
-
-
-      var ingredientsLines = response.hits[i].recipe.ingredientLines;
-          ////console.log(ingredientsLines)
    
-            var createlist= function() {
-            var list = $("<ul>");
-                reveal.append(list);
-                for (var e = 0; e < ingredientsLines.length; e++) {
-                    var itemIngredient = ingredientsLines[e];
-                    var itemList = $("<li>");
-                        itemList.text(itemIngredient);
-                        list.append(itemList);
-                    }; // Cierra for loop
-            reveal.append(list)
-             } // Cierra createList function
+    // ===== CLOSE CHIPS ===== 
+    $(document).on("click", ".close", function(){
 
-    createlist()
-
-    var source = response.hits[i].recipe.url
-        //console.log(source);
-
-    var link= $("<a>");
-        link.addClass("waves-effect waves-light btn-small");
-        link.text("Full Recipe");
-        link.attr("href", source)
-        reveal.append(link);
+        var parent = $(this).parent();
+        var name = parent.data("name");
+        console.log("CLick close");
+        var adios = ingredientsArray.splice(ingredientsArray.indexOf(name), 1);
+        console.log("adios", adios)    
+        console.log("despues de borrar", ingredientsArray)
+        console.log("ingredients strings", ingredientsString);
+        $(".recipesContainer").empty();
+        ajaxCall();
+    }); 
+        
 
 
-    var plusIcon = $("<i>");
-        plusIcon.addClass("material-icons right");
-        plusIcon.text("add");
-        link.append(plusIcon);
 
-        }; // cierra loop results.length display de los resultados
-      
+    // ====================== AJAX CALL ===============================
+
+    function ajaxCall(){
+        ingredientsString = ingredientsArray.join("-");
+        var key= "d700cd0ee0b7bf70739c9bd846d3080d"	;	
+        var queryURL = "https://api.edamam.com/search?q=" +
+        ingredientsString + "&to=12&app_id=" +  "f1e5b0de" + "&app_key=" + key;
+        
+        
+        
+        console.log(queryURL);
+                
+        // metodo ajax  para objetener de la url lo que quermos
+        $.ajax({
+        url: queryURL, 
+        method: "GET"
+        })
+        // hace una promesa. de que se cargue hasta .que lo de arriba este listo o no se ejecute mientras
+        
+        .then(function(response) {
+
+        var results = response.hits;
+        //console.log("Results", results);
+
+        for (var i = 0; i < results.length; i++) {
+                // $(".recipesContainer").empty();
+
+            var cardContainer = $("<div>");
+                cardContainer.addClass("cardContainer col s12 m6 l4 xl3");
+                $(".recipesContainer").append(cardContainer);
+
+            var singleCard = $("<div>");
+                singleCard.addClass("card sticky-action col s12");
+                cardContainer.append(singleCard);
+                
+            var imageContainer = $("<div>");
+                imageContainer.addClass("card-image waves-effect waves-block waves-light"); 
+                singleCard.append(imageContainer);
+            
+                var image = response.hits[i].recipe.image;
+                //console.log(image);
+            
+            var imageCard = $("<img>");
+                imageCard.addClass("activator");
+                imageCard.attr("src", image);
+                imageContainer.append(imageCard);
+
+            // ===== CARD CONTENT =====
+
+            var cardContent = $("<div>");
+                cardContent.addClass("card-content");
+                singleCard.append(cardContent);    
+
+
+                var nameRecipe = response.hits[i].recipe.label;
+                //console.log(nameRecipe);
+            
+            var cardTitle = $("<div>");
+                cardTitle.addClass("cardTitle");
+                cardContent.append(cardTitle);
+
+            var nombre = $("<div>");
+                nombre.addClass("card-title activator truncate flexTitle");
+                nombre.text(nameRecipe);    
+                cardTitle.append(nombre);
+
+            var iconContainer = $("<div>");
+                iconContainer.addClass("card-title activator");
+                cardTitle.append(iconContainer);
+                
+            var iconoMas = $("<i>");
+                iconoMas.addClass("material-icons small right");
+                iconoMas.text("add_circle");
+                iconContainer.append(iconoMas); 
+
+                var cookingTime = response.hits[i].recipe.totalTime;
+                //console.log(cookingTime);
+
+            var cardTime = $("<div>");
+                cardTime.addClass("cardText");
+                cardContent.append(cardTime);
+
+                if (cookingTime === 0){
+                    cookingTime = "N/A";
+                    var time = $("<p>")
+                    time.text(cookingTime);
+                    cardTime.append(time);
+
+                }else{
+                    var time = $("<p>")
+                    time.text(cookingTime + " min");
+                    cardTime.append(time);
+                };
+                
+
+            
+            var iconoReloj = $("<i>");
+                iconoReloj.addClass("material-icons left");
+                iconoReloj.text("timer");
+                time.append(iconoReloj);
+
+
+
+            var calories = response.hits[i].recipe.calories;
+            var parseCalories = parseInt(calories);
+            //console.log(calories);
+
+            var cardCalories = $("<div>");
+                cardCalories.addClass("cardText");
+                cardContent.append(cardCalories);
+
+            var calorias = $("<p>");
+                calorias.text(parseCalories + " cal");
+                cardCalories.append(calorias);
+                
+            var iconoFeliz = $("<i>");
+                iconoFeliz.addClass("material-icons left");
+                iconoFeliz.text("mood");
+                calorias.append(iconoFeliz);
+            
+            // =========== CARD REVEAL ============
+
+            var servings = response.hits[i].recipe.yield;
+            //console.log(servings);
+            
+            var reveal = $("<div>");
+                reveal.addClass("card-reveal");
+                singleCard.append(reveal);
+
+
+
+            var titleReveal = $("<div>");
+                titleReveal.addClass("card-title flexTitle truncate");
+                titleReveal.text(nameRecipe);
+                reveal.append(titleReveal);
+
+            var iconoContenedor = $("<div>");
+                iconoContenedor.addClass("card-title");
+                reveal.append(iconoContenedor);
+
+            
+            var closeReveal = $("<i>");
+                closeReveal.addClass("material-icons small right");
+                closeReveal.text("close");
+                iconoContenedor.append(closeReveal); 
+
+            var yieldServings = $("<p>");
+                yieldServings.addClass("yields");
+                yieldServings.text(servings + " servings");
+                reveal.append(yieldServings);
+
+
+            var ingredientsLines = response.hits[i].recipe.ingredientLines;
+                ////console.log(ingredientsLines)
+        
+                    var createlist= function() {
+                    var list = $("<ul>");
+                        reveal.append(list);
+                        for (var e = 0; e < ingredientsLines.length; e++) {
+                            var itemIngredient = ingredientsLines[e];
+                            var itemList = $("<li>");
+                                itemList.text(itemIngredient);
+                                list.append(itemList);
+                            }; // Cierra for loop
+                    reveal.append(list)
+                    } // Cierra createList function
+
+            createlist()
+
+            var source = response.hits[i].recipe.url
+                //console.log(source);
+
+            var link= $("<a>");
+                link.addClass("waves-effect waves-light btn-small");
+                link.text("Full Recipe");
+                link.attr("href", source)
+                link.attr("target", "_blank");
+                reveal.append(link);
+
+
+            var plusIcon = $("<i>");
+                plusIcon.addClass("material-icons right");
+                plusIcon.text("add");
+                link.append(plusIcon);
+
+            }; // cierra loop results.length display de los resultados
+        
         });  // cierra Response 
-    
-}; // Cierra Ajax Call
+        
+    }; // Cierra Ajax Call
 }); // La segunda cierra on document ready
-  
+    
